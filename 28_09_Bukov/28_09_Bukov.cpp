f859301f0f1edd662267845e4e7a6072614b8762
AndreyCorpse28090 <280990corpse@gmail.com>
2023-10-07 22:13:15 +0300

    printContact addContact

--- 28_09_Bukov/28_09_Bukov.cpp
+++ 28_09_Bukov/28_09_Bukov.cpp
@@ -1,7 +1,9 @@
 ﻿#include <iostream>
 #include <string>
+#include <Windows.h>
+#include <cstdio>
 
-class PhoneBook 
+class PhoneBook
 {
 private:
     std::string* name;
@@ -12,68 +14,126 @@ private:
 
 public:
     // Конструктор с полным набором параметров
-    PhoneBook(const std::string& nameP, unsigned int HomePhoneP, unsigned int WorkPhoneP, unsigned int MobilePhoneP, const std::string& InfoP)
-        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
-        HomePhone{ HomePhoneP },
-        WorkPhone{ WorkPhoneP },
-        MobilePhone{ MobilePhoneP },
-        Info{ (!InfoP.empty()) ? new std::string(InfoP) : nullptr }
-    {
-        std::cout << "Конструктор с полным набором параметров отработал" << std::endl;
-    }
+    PhoneBook(const std::string& nameP, unsigned int HomePhoneP, unsigned int WorkPhoneP, unsigned int MobilePhoneP, const std::string& InfoP);
 
     // Конструктор по умолчанию
-    PhoneBook()
-        : name(nullptr), HomePhone(0), WorkPhone(0), MobilePhone(0), Info(nullptr)
-    {
-        std::cout << "Конструктор по умолчанию отработал" << std::endl;
-    }
+    PhoneBook();
 
-    // конструктор с именем
-    PhoneBook(const std::string& nameP)
-        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
-        HomePhone(0), WorkPhone(0), MobilePhone(0), Info(nullptr)
-    {
-        std::cout << "Конструктор c 1 параметром отработал" << std::endl;
-    }
+    // деструктор
+    ~PhoneBook();
 
-    // конструктор с именем, дом телефоном
-    PhoneBook(const std::string& nameP, unsigned int HomePhoneP)
-        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
-        HomePhone{ HomePhoneP }, WorkPhone(0), MobilePhone(0), Info(nullptr)
-    {
-        std::cout << "Конструктор с 2 параметрами отработал" << std::endl;
-    }
+    // Метод для отображения контакта
+    void printContact() const;
 
-    // конструктор именем, домашним, рабочим телефоном
-    PhoneBook(const std::string& nameP, unsigned int HomePhoneP, unsigned int WorkPhoneP)
-        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
-        HomePhone{ HomePhoneP }, WorkPhone{ WorkPhoneP }, MobilePhone(0), Info(nullptr)
-    {
-        std::cout << "Конструктор с 3 параметрами отработал" << std::endl;
-    }
-
-    // конструктор с именем, домашним, рабочим и мобильным телефоном
-    PhoneBook(const std::string& nameP, unsigned int HomePhoneP, unsigned int WorkPhoneP, unsigned int MobilePhoneP)
-        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
-        HomePhone{ HomePhoneP }, WorkPhone{ WorkPhoneP }, MobilePhone{ MobilePhoneP }, Info(nullptr)
-    {
-        std::cout << "Конструктор с 4 параметрами отработал" << std::endl;
-    }
-
-    // дестуктор
-    ~PhoneBook()
-    {
-        delete name;
-        delete Info;
-    }
-
-
+    // Метод для добавления контакта
+    void addContact(const std::string& contactName, unsigned int homePhone, unsigned int workPhone, unsigned int mobilePhone, const std::string& info);
 };
 
-int main()
-{
+
+int main() {
     setlocale(LC_ALL, "rus");
-    PhoneBook per1;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    PhoneBook phoneBook;
+
+    while (true) {
+        std::cout << "Выберите операцию:" << std::endl;
+        std::cout << "1. Добавить контакт" << std::endl;
+        std::cout << "2. Показать контакт" << std::endl;
+        std::cout << "3. Выйти" << std::endl;
+
+        int choice;
+        std::cin >> choice;
+
+        if (choice == 3) {
+            break;
+        }
+
+        switch (choice) {
+        case 1: {
+            unsigned int homePhone, workPhone, mobilePhone;
+            std::string info;
+
+            std::cin.ignore();
+            std::cout << "Введите ФИО: ";
+            std::string contactName;
+            std::getline(std::cin, contactName);
+
+            std::cout << "Введите домашний телефон: ";
+            std::cin >> homePhone;
+
+            std::cout << "Введите рабочий телефон: ";
+            std::cin >> workPhone;
+
+            std::cout << "Введите мобильный телефон: ";
+            std::cin >> mobilePhone;
+
+            std::cin.ignore();
+            std::cout << "Введите дополнительную информацию: ";
+            std::getline(std::cin, info);
+
+            phoneBook.addContact(contactName, homePhone, workPhone, mobilePhone, info);
+            std::cout << "Контакт '" << contactName << "' добавлен в телефонную книгу." << std::endl;
+            break;
+        }
+        case 2: {
+            phoneBook.printContact();
+            break;
+        }
+        default:
+            std::cout << "Некорректный выбор. Попробуйте еще раз." << std::endl;
+        }
+    }
+
     return 0;
+}
+
+// Реализация конструктора с полным набором параметров
+PhoneBook::PhoneBook(const std::string& nameP, unsigned int HomePhoneP, unsigned int WorkPhoneP, unsigned int MobilePhoneP, const std::string& InfoP)
+    : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
+    HomePhone{ HomePhoneP },
+    WorkPhone{ WorkPhoneP },
+    MobilePhone{ MobilePhoneP },
+    Info{ (!InfoP.empty()) ? new std::string(InfoP) : nullptr } {}
+
+// Реализация конструктора по умолчанию
+PhoneBook::PhoneBook()
+    : name(nullptr), HomePhone(0), WorkPhone(0), MobilePhone(0), Info(nullptr) {}
+
+// Реализация деструктора
+PhoneBook::~PhoneBook() {
+    delete name;
+    delete Info;
+}
+
+// Реализация метода для отображения контакта
+void PhoneBook::printContact() const {
+    if (name != nullptr && Info != nullptr) {
+        std::cout << "Имя: " << *name << std::endl;
+        std::cout << "Домашний телефон: " << HomePhone << std::endl;
+        std::cout << "Рабочий телефон: " << WorkPhone << std::endl;
+        std::cout << "Мобильный телефон: " << MobilePhone << std::endl;
+        std::cout << "Дополнительная информация: " << *Info << std::endl;
+    }
+    else {
+        std::cout << "Контакт не содержит данных." << std::endl;
+    }
+}
+
+// Реализация метода для добавления контакта
+void PhoneBook::addContact(const std::string& contactName, unsigned int homePhone, unsigned int workPhone, unsigned int mobilePhone, const std::string& info) {
+    if (name != nullptr) {
+        delete name;
+        name = nullptr;
+    }
+    if (Info != nullptr) {
+        delete Info;
+        Info = nullptr;
+    }
+
+    name = new std::string(contactName);
+    HomePhone = homePhone;
+    WorkPhone = workPhone;
+    MobilePhone = mobilePhone;
+    Info = new std::string(info);
 }
