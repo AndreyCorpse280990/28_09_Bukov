34df941a7353a8fa28357d6fdbbb7b9478ac70fd
AndreyCorpse28090 <280990corpse@gmail.com>
2023-10-07 18:53:00 +0300

    1

--- 28_09_Bukov/28_09_Bukov.cpp
+++ 28_09_Bukov/28_09_Bukov.cpp
@@ -1,20 +1,79 @@
-﻿// 28_09_Bukov.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+class PhoneBook 
+{
+private:
+    std::string* name;
+    unsigned int HomePhone;
+    unsigned int WorkPhone;
+    unsigned int MobilePhone;
+    std::string* Info;
+
+public:
+    // Конструктор с полным набором параметров
+    PhoneBook(const std::string& nameP, unsigned int HomePhoneP, unsigned int WorkPhoneP, unsigned int MobilePhoneP, const std::string& InfoP)
+        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
+        HomePhone{ HomePhoneP },
+        WorkPhone{ WorkPhoneP },
+        MobilePhone{ MobilePhoneP },
+        Info{ (!InfoP.empty()) ? new std::string(InfoP) : nullptr }
+    {
+        std::cout << "Конструктор с полным набором параметров отработал" << std::endl;
+    }
+
+    // Конструктор по умолчанию
+    PhoneBook()
+        : name(nullptr), HomePhone(0), WorkPhone(0), MobilePhone(0), Info(nullptr)
+    {
+        std::cout << "Конструктор по умолчанию отработал" << std::endl;
+    }
+
+    // конструктор с именем
+    PhoneBook(const std::string& nameP)
+        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
+        HomePhone(0), WorkPhone(0), MobilePhone(0), Info(nullptr)
+    {
+        std::cout << "Конструктор c 1 параметром отработал" << std::endl;
+    }
+
+    // конструктор с именем, дом телефоном
+    PhoneBook(const std::string& nameP, unsigned int HomePhoneP)
+        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
+        HomePhone{ HomePhoneP }, WorkPhone(0), MobilePhone(0), Info(nullptr)
+    {
+        std::cout << "Конструктор с 2 параметрами отработал" << std::endl;
+    }
+
+    // конструктор именем, домашним, рабочим телефоном
+    PhoneBook(const std::string& nameP, unsigned int HomePhoneP, unsigned int WorkPhoneP)
+        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
+        HomePhone{ HomePhoneP }, WorkPhone{ WorkPhoneP }, MobilePhone(0), Info(nullptr)
+    {
+        std::cout << "Конструктор с 3 параметрами отработал" << std::endl;
+    }
+
+    // конструктор с именем, домашним, рабочим и мобильным телефоном
+    PhoneBook(const std::string& nameP, unsigned int HomePhoneP, unsigned int WorkPhoneP, unsigned int MobilePhoneP)
+        : name{ (!nameP.empty()) ? new std::string(nameP) : nullptr },
+        HomePhone{ HomePhoneP }, WorkPhone{ WorkPhoneP }, MobilePhone{ MobilePhoneP }, Info(nullptr)
+    {
+        std::cout << "Конструктор с 4 параметрами отработал" << std::endl;
+    }
+
+    // дестуктор
+    ~PhoneBook()
+    {
+        delete name;
+        delete Info;
+    }
+
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+    PhoneBook per1;
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
